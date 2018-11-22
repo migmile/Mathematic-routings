f5c80c3ed1b8c84d0180e3c3478096b6c69db9dd
Evgeniy Gnesin <intermigmile@hotmail.com>
2018-11-22 15:07:24 +0300

    Update simpson.cpp
    заработало :)

--- Mathematics/Integr/simpson.cpp
+++ Mathematics/Integr/simpson.cpp
@@ -1,4 +1,4 @@
-#include <math.h>
+#include <cmath>
 #include <iostream>
 using namespace std;
 
@@ -6,16 +6,28 @@ using namespace std;
     double Simpson( const double a,const double b,double (*f)(double x),const int N)
     {
        double xp,x,xn;
-       double I=0;
+       double I=0,I1=0;
        double h=(b-a)/N;
-       xp=a;
-       for (int i=1;i<N-1;i+=2)
+       x=a+h;
+       for (int i=1;i<N;i+=2)
        {
-        x=xp+h;
-        xn=x+h;
-        I+= ( f(xp)+4.*f(x)+f(xn) );
-        xp=x;
+		I1 += f(x);
+		std::cout << x << " " << I1 << "\n";
+		x += (h + h);
        }
+	   I1 *= 4;
+	   std::cout << "\n";
+	   I = 0;
+	   x = a + h+h;
+	   for (int i = 1; i < N-1; i += 2)
+	   {
+		   I += f(x);
+		   std::cout << x << " " << I << "\n";
+		   x += (h + h);
+	   }
+	   I *= 2;
+
+	   I = f(a) + f(b)+I+I1;
       return I*h/3.;
     }
 
@@ -39,14 +51,18 @@ double Simpson1(double a, double b, double eps, double (*f)(double))
                 sum2 += (index2 = 6.-index2)*func;
             };
         };
-        if (fabs((sum1-2.*sum2)/sum1)<=eps) return (h*sum1)/3.+h*(2.*sum2-sum1)/45.; else N*=2.;
+		if (fabs((sum1 - 2.*sum2) / sum1) <= eps) return (h*sum1) / 3. + h * (2.*sum2 - sum1) / 45.; else
+		{
+			std::cout << N << "   :" << sum1 << "  " << sum2 << "\n";
+			N *= 2.;
+		}
     };
     return (h*sum1)/3.;
 };
 
 double fun(double x)
 {
- return sin(x);
+	return x / (x*x*x*x + 4);// sin(x);
 }
 
 int main()
@@ -59,7 +75,7 @@ int main()
  std::cout<<Simpson(0.,3.14159,fun,80)<<"\n"; 
  std::cout<<Simpson(0.,3.14159,fun,100)<<"\n"; 
 */
- std::cout<<Simpson(0.,3.14159,fun,800)<<"\n"; 
-  std::cout<<Simpson1(0.,3.14159, 0.001, fun);
+ std::cout<<Simpson(0.,5.,fun,10)<<"\n"; 
+//  std::cout<<Simpson1(0.,3.14159, 0.001, fun);
  return 0;
 }
